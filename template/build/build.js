5d8ce0c39fbbc015a6058fbe0440aca41bf40882
sky-admin <546485299@qq.com>
2018-12-12 15:43:58 +0800

    fix bug in plugin mode

--- template/build/build.js
+++ template/build/build.js
@@ -17,11 +17,12 @@ program
 
 function runWebpack (cfg) {
   if (program.production || program.watch) {
-    cfg = merge(cfg, program.production ? {
-      mode: 'production'
-    } : {
-      cache: true
-    })
+    const extendCfg = program.production ? { mode: 'production' } : { cache: true }
+    if (Array.isArray(cfg)) {
+      cfg = cfg.map(item => merge(item, extendCfg))
+    } else {
+      cfg = merge(cfg, extendCfg)
+    }
   }
   if (process.env.npm_config_report) {
     var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
