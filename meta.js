5089da8aaa9b955a2cffe088b4c5375bc7d9319d
sky-admin <546485299@qq.com>
2019-02-26 16:18:39 +0800

    add baidu mini program choice

--- meta.js
+++ meta.js
@@ -4,7 +4,7 @@ module.exports = {
       type: 'list',
       required: true,
       message: 'Project mode',
-      choices: ['wx', 'ali'],
+      choices: ['wx', 'ali', 'swan'],
       default: 'wx'
     },
     name: {
