69fbce361e02863be8a49422d4f179df15202018
sky-admin <546485299@qq.com>
2019-07-11 10:33:39 +0800

    fix bug in test script

--- testWxCross.js
+++ testWxCross.js
@@ -2,6 +2,6 @@ const shell = require('shelljs')
 
 shell.exec('cp testfile/meta.js .')
 
-shell.exec('mpx init demo-wx-cross --offline')
+shell.exec('mpx init demo-wx-cross --offline .')
 
 shell.exec('cd demo-wx-cross && npm i && npm run build')
