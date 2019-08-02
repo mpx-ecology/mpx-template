3a9bf3e81da1a3819c2f0c72768624ef733c69cd
sky-admin <546485299@qq.com>
2019-08-02 20:12:29 +0800

    enhance test case

--- testWxNoCross.js
+++ testWxNoCross.js
@@ -1,7 +1,16 @@
 const shell = require('shelljs')
+const assert = require('assert')
+const fs = require('fs')
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 
 shell.exec('cp testfile/wx-no-cross-meta.js ./meta.js')
 
 shell.exec('mpx init demo-wx-no-cross --offline .')
 
-shell.exec('cd demo-wx-no-cross && npm i && npm run build && npm run build:cross')
+shell.exec('cd demo-wx-no-cross && npm i && npm run build')
+
+assert(fs.existsSync(resolve('demo-wx-no-cross/dist/project.config.json')))
