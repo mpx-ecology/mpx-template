e10eed2b8a9506b1696c7f512316dac973bf7600
sky-admin <546485299@qq.com>
2019-08-08 16:59:54 +0800

    test for fail

--- testWxCross.js
+++ testWxCross.js
@@ -7,14 +7,10 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 
-try {
-  shell.exec('cp testfile/wx-cross-meta.js ./meta.js')
-
-  shell.exec('mpx init demo-wx-cross --offline .')
-
-  shell.exec('cd demo-wx-cross && npm i && npm run build && npm run build:cross')
-} catch (e) {
-  process.exit(1)
-}
+shell.exec(`
+  cp testfile/wx-cross-meta.js ./meta.js &&
+  mpx init demo-wx-cross --offline . &&
+  cd demo-wx-cross && npm i && npm run build && npm run build:cross
+`).code !== 0 && shell.exit(1)
 
 assert(fs.existsSync(resolve('demo-wx-cross/dist/wx/project.config.json')))
