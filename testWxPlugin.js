0a0f7ae70e4cf16732e8627c760598667859b8eb
sky-admin <546485299@qq.com>
2019-08-12 16:04:29 +0800

    update test script

--- testWxPlugin.js
+++ testWxPlugin.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs')
 
-shell.exec('cp testfile/wx-plugin-meta.js ./meta.js')
-
-shell.exec('mpx init demo-wx-plugin --offline .')
-
-shell.exec('cd demo-wx-plugin && npm i && npm run build')
+shell.exec(`
+  cp testfile/wx-plugin-meta.js ./meta.js &&
+  mpx init demo-wx-plugin --offline . &&
+  cd demo-wx-plugin && npm i && npm run build
+`).code !== 0 && shell.exit(1)
