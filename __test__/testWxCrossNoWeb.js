e06a68b1b7b9f70aa04c6938292ba6a6a6d5179e
sky-admin <546485299@qq.com>
2019-12-04 13:21:06 +0800

    update folder name

--- __test__/testWxCrossNoWeb.js
+++ __test__/testWxCrossNoWeb.js
@@ -10,18 +10,18 @@ function resolve (dir) {
 // 复制预设的问题答案，创建项目并尝试构建要求无error
 shell.exec(`
   cp testfile/wx-cross-noweb-meta.js ./meta.js &&
-  mpx init demo-wx-cross --offline . &&
-  cd demo-wx-cross && npm i && npm run build && npm run build:cross
+  mpx init demo-wx-cross-noweb --offline . &&
+  cd demo-wx-cross-noweb && npm i && npm run build && npm run build:cross
 `).code !== 0 && shell.exit(1)
 
 // 检查相应平台的配置文件是否在对的位置
-assert(fs.existsSync(resolve('demo-wx-cross/dist/wx/project.config.json')))
+assert(fs.existsSync(resolve('demo-wx-cross-noweb/dist/wx/project.config.json')))
 
-assert(fs.existsSync(resolve('demo-wx-cross/static/wx/project.config.json')))
+assert(fs.existsSync(resolve('demo-wx-cross-noweb/static/wx/project.config.json')))
 
-assert(fs.existsSync(resolve('demo-wx-cross/dist/ali/mini.project.json')))
+assert(fs.existsSync(resolve('demo-wx-cross-noweb/dist/ali/mini.project.json')))
 
-assert(fs.existsSync(resolve('demo-wx-cross/static/ali/mini.project.json')))
+assert(fs.existsSync(resolve('demo-wx-cross-noweb/static/ali/mini.project.json')))
 
 // 检查跨平台下不应该在根目录有配置文件
-assert(!fs.existsSync(resolve('demo-wx-cross/project.config.json')))
+assert(!fs.existsSync(resolve('demo-wx-cross-noweb/project.config.json')))
