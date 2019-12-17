c545af5ddb84f80acfc578beadcf4b14080f98f0
sky-admin <546485299@qq.com>
2019-12-17 21:30:12 +0800

    fix bug

--- template/build/build.js
+++ template/build/build.js
@@ -18,9 +18,60 @@ const mainSubDir = '{% if isPlugin %}miniprogram{% elif cloudFunc %}miniprogram{
 function resolveDist (file, subPathStr = mainSubDir) {
   return path.resolve(__dirname, '../dist', subPathStr, file || '')
 }
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 const webpackConfigArr = []
 const userSelectedMode = '<$ mode $>'
+
+const mpxLoaderConfig = {
+  transRpx: {
+    mode: 'only',
+    comment: 'use rpx',
+    include: resolve('src')
+  }
+}
+
+{% if transWeb %}
+ const transWebModuleRules = [
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader'
+  },
+  {
+    test: /\.mpx$/,
+    resourceQuery: /(app|page|component)/,
+    use: [
+      {
+        loader: 'vue-loader',
+        options: {
+          transformToRequire: {
+            'mpx-image': 'src',
+            'mpx-audio': 'src',
+            'mpx-video': 'src'
+          }
+        }
+      },
+      MpxWebpackPlugin.loader(mpxLoaderConfig)
+    ]
+  },
+  {
+    test: /\.styl$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'stylus-loader'
+    ]
+  }
+]
+{% endif %}
+const transModuleRules = [
+  {
+    test: /\.mpx$/,
+    use: MpxWebpackPlugin.loader(mpxLoaderConfig)
+  }
+]
 
 {% if mode === 'wx' and not cross %}
 // 微信小程序需要拷贝project.config.json，如果npm script参数里有--wx，拷贝到/dist下，如果指定--wx，拷贝到/dist/wx下
@@ -74,6 +125,7 @@ modeArr.forEach(item => {
     output: {
       path: resolveDist('', item)
     },
+    module: {% if transWeb %}item === 'web' ? transWebModuleRules : transModuleRules{% else %}transModuleRules{% endif %},
     plugins: [
       new MpxWebpackPlugin(Object.assign({
         mode: item,
