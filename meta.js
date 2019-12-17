7d7abfdc51d5432e4d15de1158068efae08814cb
sky-admin <546485299@qq.com>
2019-12-17 14:13:55 +0800

    format code style

--- meta.js
+++ meta.js
@@ -1,5 +1,12 @@
 const realMeta = require('./realMeta')
 
 module.exports = {
+  mock: {
+    mode: 'wx',
+    cross: true,
+    transWeb: true,
+    isPlugin: false,
+    needEslint: true
+  },
   ...realMeta
 }
