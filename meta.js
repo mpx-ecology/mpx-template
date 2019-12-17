f992c9573e226470da1c9f0c3d40af697bdd68b0
sky-admin <546485299@qq.com>
2019-12-17 14:15:33 +0800

    revert mistake

--- meta.js
+++ meta.js
@@ -1,12 +1,5 @@
 const realMeta = require('./realMeta')
 
 module.exports = {
-  mock: {
-    mode: 'wx',
-    cross: true,
-    transWeb: true,
-    isPlugin: false,
-    needEslint: true
-  },
   ...realMeta
 }
