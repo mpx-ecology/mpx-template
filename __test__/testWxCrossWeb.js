9a65728bad1625f094b6eb2c78f3873693b408c5
sky-admin <546485299@qq.com>
2020-01-17 18:11:17 +0800

    update test case

--- __test__/testWxCrossWeb.js
+++ __test__/testWxCrossWeb.js
@@ -10,12 +10,15 @@ function resolve (dir) {
 // 复制预设的问题答案，创建项目并尝试构建要求无error
 shell.cp('testfile/wx-cross-web-meta.js', './meta.js').code !== 0 && shell.exit(1)
 shell.exec('mpx init demo-wx-cross-web --offline .').code !== 0 && shell.exit(1)
-shell.exec('cd demo-wx-cross-web && npm i && npm run build && npm run build:cross').code !== 0 && shell.exit(1)
+shell.exec('cd demo-wx-cross-web && npm i && npm run build').code !== 0 && shell.exit(1)
 
 // 检查相应平台的配置文件是否在对的位置
 assert(fs.existsSync(resolve('demo-wx-cross-web/dist/wx/project.config.json')))
 
 assert(fs.existsSync(resolve('demo-wx-cross-web/static/wx/project.config.json')))
+assert(!fs.existsSync(resolve('demo-wx-cross-web/static/wx/index.html')))
+
+shell.exec('npm run build:cross').code !== 0 && shell.exit(1)
 
 assert(fs.existsSync(resolve('demo-wx-cross-web/dist/ali/mini.project.json')))
 
