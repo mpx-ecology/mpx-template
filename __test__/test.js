1461467c3460986ffe987e9a34b7ab7b176fd625
sky-admin <546485299@qq.com>
2020-02-16 22:28:44 +0800

    update

--- __test__/test.js
+++ __test__/test.js
@@ -1,17 +1,18 @@
-const { exec } = require('child_process');
+const { exec } = require('child_process')
+const bddStdin = require('bdd-stdin')
 
 const initProjectRunner = exec('mpx init demo-wx-cross-web')
 
 // initProjectRunner.stdout.setEncoding('utf8')
 
 initProjectRunner.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+  console.log(`stdout: ${data}`)
   if (data.includes('请选择小程序项目所属平台（目前仅微信下支持跨平台输出）')) {
-    initProjectRunner.stdin.write('\40')
+    bddStdin(bddStdin.keys.down, '\n')
   }
   // initProjectRunner.stdin.write('\n')
-});
+})
 
 initProjectRunner.stdout.on('pause', () => {
-  console.log('暂停');
-});
+  console.log('暂停')
+})
