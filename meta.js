5ba3541c96f1b349999018e859bd1937a7a3408a
Huanyu Li <lhy950424@gmail.com>
2020-06-10 16:02:40 +0800

    fix

--- meta.js
+++ meta.js
@@ -1,13 +1,5 @@
 const realMeta = require('./realMeta')
 
 module.exports = {
-  mock: {
-    mode: 'wx',
-    cross: false,
-    isPlugin: true,
-    needEslint: true,
-    babel7Support: false,
-    needUnitTest: false
-  },
   ...realMeta
 }
