62091a52f97ecedf1c1dd985ac9e54162c481a87
sky-admin <546485299@qq.com>
2020-08-15 12:34:51 +0800

    fix bug

--- __test__/testWxNoCross.js
+++ __test__/testWxNoCross.js
@@ -1,16 +1,15 @@
 const shell = require('shelljs')
-const assert = require('assert')
-const fs = require('fs')
 const path = require('path')
+const { checkStatic } = require('./util')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
+const projectFolder = resolve('demo-wx-no-cross')
+
 shell.cp('testfile/wx-no-cross-meta.js', './meta.js').code !== 0 && shell.exit(1)
 shell.exec('mpx init demo-wx-no-cross --offline .').code !== 0 && shell.exit(1)
 shell.exec('cd demo-wx-no-cross && npm i && npm run build').code !== 0 && shell.exit(1)
 
-assert(fs.existsSync(resolve('demo-wx-no-cross/dist/project.config.json')))
-assert(fs.existsSync(resolve('demo-wx-no-cross/project.config.json')))
-assert(!fs.existsSync(resolve('demo-wx-no-cross/static/wx/project.config.json')))
+checkStatic(projectFolder, {isCross: true})
