1fef9f2e1b51b24edc71fc5c9914cac5a985cb49
hiyuki <674883329@qq.com>
2020-11-09 19:47:44 +0800

    fixfix

--- __test__/test-dll-cross.js
+++ __test__/test-dll-cross.js
@@ -6,7 +6,7 @@ const context = resolve()
 const projectContext = resolve(name)
 
 shell.exec(`rm -rf ${projectContext}`).code !== 0 && shell.exit(1)
-shell.exec(`cd ${context} && ./node_modules/.bin/mpx init ${name} --offline . --mock needDll,cross`).code !== 0 && shell.exit(1)
+shell.exec(`cd ${context} && mpx init ${name} --offline . --mock needDll,cross`).code !== 0 && shell.exit(1)
 shell.exec(`cd ${projectContext} && npm i && npm run build:dll && npm run build:cross`).code !== 0 && shell.exit(1)
 
 checkFiles(projectContext, ['dist/wx', 'dist/ali', 'dll/lib'])
