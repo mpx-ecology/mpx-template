913ed965966e20a0f7152e5ef1e1c01832dea41a
xiaowen Jia <jxw619@sina.com>
2021-10-14 16:22:13 +0800

    接入e2e SDK

--- template/test/e2e/index.spec.js
+++ template/test/e2e/index.spec.js
@@ -3,7 +3,7 @@
  * 首先开启工具安全设置中的 CLI/HTTP 调用功能
  * docs of miniprogram-automator: https://developers.weixin.qq.com/miniprogram/dev/devtools/auto/quick-start.html
  */
-const automator = require('miniprogram-automator')
+import automator from '@didi/e2e-extension'
 const path = require('path')
 
 describe('index', () => {
@@ -19,7 +19,7 @@ describe('index', () => {
   }, 30000)
 
   it('desc', async () => {
-    const desc = await page.$('.list--list')
+    const desc = await page.$('list--list')
     expect(desc.tagName).toBe('view')
     expect(await desc.text()).toContain('手机')
   })
