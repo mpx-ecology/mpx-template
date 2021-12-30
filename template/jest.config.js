24b6a8347564ed6a77c58ff31152a05b22c8609e
xuegan <xuegan@didiglobal.com>
2021-12-30 18:30:48 +0800

    修改mpx-jest版本

--- template/jest.config.js
+++ template/jest.config.js
@@ -5,8 +5,10 @@
 module.exports = {
   // rootDir: path.join(__dirname),
   moduleFileExtensions: ['js', 'mpx', 'json'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     // webpack的alias需要在此处理
+    '^src(.*)': '<rootDir>/src/$1'
   },
   testPathIgnorePatterns: ['dist', 'node_modules'],
   testURL: 'http://test.api.com',
