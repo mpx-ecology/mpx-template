91323dbbc3127c044c269d3695784370a1a98cf0
pagnkelly <632228198@qq.com>
2022-09-02 17:19:57 +0800

    feat: 增加eslint相应文件文档

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
   extends: ['@mpxjs'],
   rules: {
-    // .mpx文件规则
+    // .mpx文件规则 https://mpx-ecology.github.io/eslint-plugin-mpx/rules/
   },
   overrides: [
     {
       files: ['**/*.ts'],
       rules: {
-        // .ts文件规则
+        // .ts文件规则 https://typescript-eslint.io/rules/
+        
       }
     },
     {
       files: ['**/*.js'],
       rules: {
-        // .js文件规则
+        // .js文件规则 https://eslint.bootcss.com/docs/rules/
       }
     }
   ]
